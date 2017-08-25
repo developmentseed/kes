dd0cf42758c0ff9cc044402b6c9696eb8755ba81
Scisco <scisco7@gmail.com>
2017-08-25 13:22:42 -0400

    remove unused modules

--- index.js
+++ index.js
@@ -3,8 +3,6 @@
 module.exports = {
   common: require('./src/common'),
   lambda: require('./src/lambda'),
-  dynamo: require('./src/dynamo'),
   envs: require('./src/envs'),
-  bootstrap: require('./src/bootstrap'),
   CF: require('./src/cf')
 };
