da03a665cfdf48dc469481f05caaeb9c868f3c73
Scisco <scisco7@gmail.com>
2017-08-29 16:18:25 -0400

    docstring

--- src/local.js
+++ src/local.js
@@ -2,6 +2,21 @@
 
 const isLocal = process.argv[2] === 'local';
 
+/**
+ * A simple helper for running a function if `local` is passed as argument
+ * @example
+ * // test.js
+ * const { localRun } = require('kes');
+ * localRun(() => {
+ *   console.log('my function');
+ * });
+ * // result
+ * // $ node test.js local
+ * // my function
+ *
+ * @param {Function} func A javascript function
+ * @return {Object} returns the result of the function call
+ */
 module.exports.localRun = (func) => {
   if (isLocal) {
     // Run the function
